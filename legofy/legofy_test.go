66baa317126527bf0f8e55ec9512e3c79ea001e5
Win Htaik Aung <winhtaikaung28@hotmail.com>
2019-04-05 17:24:26 +0800

    legofy basic added

---
+++ legofy/legofy_test.go
@@ -0,0 +1,21 @@
+package legofy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetLegoPallette(*testing.T) {
+	fmt.Println("TestGel Logo Pallette")
+}
+
+func TestApplyThumbnNailEffects(*testing.T) {
+	fmt.Println("Test ApplyThumbnail Pallette")
+}
+
+func TestGetNewSize(*testing.T) {
+	fmt.Println("Test GetNewSize")
+}
+func TestLegofyImage(*testing.T) {
+	fmt.Println("TestLegofyImage")
+}
